c983dd7d2913379a97795505e6b1197ea970d293
adt1710 <adrianoibira.adt@gmail.com>
2022-11-14 10:03:45 -0300

    Melhorias de interação aplicadas.

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ def jogar():
                     "2-) Papel\n"
                     "3-) Tesoura\n"
                     "R: ")
+    printa_escolha("Jogador", usuario)
 
     computador = random.choice(['1', '2', '3'])
+    printa_escolha("Computador", computador)
+
     if usuario == computador:
         return print('Empate')
 
@@ -15,6 +18,11 @@ def jogar():
         return print("Você ganhou!")
 
     print("Você perdeu!")
+
+def printa_escolha(jogador,escolha):
+    if escolha == "1": return print(f'{jogador} escolheu pedra.')
+    elif escolha == "2": return print(f'{jogador} escolheu papel.')
+    elif escolha == "3": return print(f'{jogador} escolheu tesoura.')
 
 def ganhou(jogador, oponente):
     if (jogador == '1' and oponente == '3') or (jogador == '2' and oponente == '1') or (jogador == '3' and oponente == '2'):
